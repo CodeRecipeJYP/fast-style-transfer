a0a8ecceb921cb30540637e6fa8946f79854ded5
pjo901018 <pjo901018@korea.ac.kr>
2017-10-05 00:46:02 +0900

    Adding VGGPATH flag

--- start_test.sh
+++ start_test.sh
@@ -12,7 +12,10 @@ TEST=$CONTENT_DIR$CONTENT_FILENAME
 TEST_DIR=tests/
 CKPT_DIR=/media/ubuntu/a5d0dd63-d6a7-4d32-b068-0978b9fa66fb/171005_music/ckpts
 
-CMD="nohup python3 style_test.py --style $STYLE --checkpoint-dir $CKPT_DIR --test $TEST --test-dir $TEST_DIR"
+VGG_PATH=/media/ubuntu/bc8228a2-528b-4e89-a7b6-4fa1a7a10526/fast-style-transfer-forked/data/imagenet-vgg-verydeep-19.mat
+
+
+CMD="nohup python3 style_test.py --style $STYLE --checkpoint-dir $CKPT_DIR --test $TEST --test-dir $TEST_DIR --vgg-path $VGG_PATH"
 
 echo $CMD
 $CMD
